48d4ea996b4baea894b628d48883b450df42c6d0
hosnyben <housnibenabid@gmail.com>
2015-09-24 21:08:34 +0100

    Set news options

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# equalHeight
+# equalHeight v1.1
 an easy jQuery Plugin to equal elements height.
+
+==========================================================
+For more suggestions you can contact me at :
+
+- facebook : https://www.facebook.com//hosny707
+- gmail : housnibenabid@gmail.com
+- LinkedIn : https://ma.linkedin.com/in/housnibenabid
+==========================================================
+
+Plugin details :
+
+-> Default settings : 
+$(element).equalHeight({
+	dimension : "none",
+	max_width : 0,
+	min_width : 0,
+})
+
+-> Description : 
+	dimension : set if you want the equalHeight works for innerHeight outerHeight or only height
+				none : height
+				outer : outerHeight
+				inner : innerHeight
+
+	max_width : set the max window width to run this plugin (0: disable option)
+
+	min_width : set the min window width to run this plugin (0: disable option)
+
--- equalHeight.js
+++ equalHeight.js
@@ -1,12 +1,31 @@
 (function($){
-	$.fn.equalHeight = function(){
+	$.fn.equalHeight = function(options){
 		var init = 0;
-		this.each(function() {
-    		$(this).height("")
-			if(init <= $(this).height())
-				init = $(this).height();
-		})
 
-		return this.height(init);
+		var default_settings = {
+			max_width : 0,
+			min_width : 0,
+			dimension : "none"
+		}
+
+		var settings = $.extend(default_settings,options);
+
+		if((settings.min_width && settings.max_width && $(window).innerWidth()>=settings.min_width && $(window).innerWidth()<=settings.max_width) || (settings.min_width && !settings.max_width && $(window).innerWidth()>=settings.min_width) || (!settings.min_width && settings.max_width && $(window).innerWidth()<=settings.max_width)){
+			this.each(function() {
+	    		$(this).height("")
+				if(init <= $(this).height())
+					init = $(this).height();
+			})
+
+			if(settings.dimension=="outer")
+				return this.outerHeight(init);
+			else if(settings.dimension=="inner")
+				return this.innerHeight(init);
+			else
+				return this.height(init);
+
+		}else{
+			return this.height("");
+		}
 	}
 })(jQuery);
