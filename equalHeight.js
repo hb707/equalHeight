a80ffdd45645148ca4b7ad5e7311101e4ab153b5
Hosny BEN <housnibenabid@gmail.com>
2015-11-25 18:17:27 +0000

    Update conditions V1.1.1

--- equalHeight.js
+++ equalHeight.js
@@ -11,7 +11,12 @@
 
 		var settings = $.extend(default_settings,options);
 
-		if((settings.min_width && settings.max_width && $(window).innerWidth()>=settings.min_width && $(window).innerWidth()<=settings.max_width) || (settings.min_width && !settings.max_width && $(window).innerWidth()>=settings.min_width) || (!settings.min_width && settings.max_width && $(window).innerWidth()<=settings.max_width) || (!settings.min_width && !settings.max_width)){
+		var condition01 = settings.min_width!=0 && settings.max_width!=0 && $(window).innerWidth()>=settings.min_width && $(window).innerWidth()<=settings.max_width),
+			condition02 = settings.min_width!=0 && settings.max_width==0 && $(window).innerWidth()>=settings.min_width),
+			condition03 = settings.min_width==0 && settings.max_width!=0 && $(window).innerWidth()<=settings.max_width),
+			condition04 = settings.min_width==0 && settings.max_width==0;
+
+		if(condition01 || condition02 || condition03 || condition04){
 			this.each(function() {
 	    		$(this).height("")
 				if(init <= $(this).height() && settings.referer == "max" || init >= $(this).height() && settings.referer == "min")
