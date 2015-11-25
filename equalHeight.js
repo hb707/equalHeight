ff64110950ef91d538bacd020a64306206d7e5ea
Hosny BEN <housnibenabid@gmail.com>
2015-11-25 18:22:16 +0000

    syntax error

--- equalHeight.js
+++ equalHeight.js
@@ -11,9 +11,9 @@
 
 		var settings = $.extend(default_settings,options);
 
-		var condition01 = settings.min_width!=0 && settings.max_width!=0 && $(window).innerWidth()>=settings.min_width && $(window).innerWidth()<=settings.max_width),
-			condition02 = settings.min_width!=0 && settings.max_width==0 && $(window).innerWidth()>=settings.min_width),
-			condition03 = settings.min_width==0 && settings.max_width!=0 && $(window).innerWidth()<=settings.max_width),
+		var condition01 = settings.min_width!=0 && settings.max_width!=0 && $(window).innerWidth()>=settings.min_width && $(window).innerWidth()<=settings.max_width,
+			condition02 = settings.min_width!=0 && settings.max_width==0 && $(window).innerWidth()>=settings.min_width,
+			condition03 = settings.min_width==0 && settings.max_width!=0 && $(window).innerWidth()<=settings.max_width,
 			condition04 = settings.min_width==0 && settings.max_width==0;
 
 		if(condition01 || condition02 || condition03 || condition04){
