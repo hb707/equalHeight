3a55e9f5b7aaebbffaf85571bdadde7935b4b333
Hosny BEN <housnibenabid@gmail.com>
2016-08-17 03:56:21 +0100

    Update of equalHeight

--- equalHeight.js
+++ equalHeight.js
@@ -1,37 +1,40 @@
 (function($){
 	$.fn.equalHeight = function(options){
-		var init = 0;
+		var element = this;
+		$(window).on("resize",function(){
+			var init = 0;
 
-		var default_settings = {
-			max_width 	: 0,
-			min_width 	: 0,
-			dimension 	: "none",
-			referer		: "max"
-		}
+			var default_settings = {
+				max_width 	: 0,
+				min_width 	: 0,
+				dimension 	: "none",
+				referer		: "max"
+			}
 
-		var settings = $.extend(default_settings,options);
+			var settings = $.extend(default_settings,options);
 
-		var condition01 = settings.min_width!=0 && settings.max_width!=0 && $(window).innerWidth()>=settings.min_width && $(window).innerWidth()<=settings.max_width,
-			condition02 = settings.min_width!=0 && settings.max_width==0 && $(window).innerWidth()>=settings.min_width,
-			condition03 = settings.min_width==0 && settings.max_width!=0 && $(window).innerWidth()<=settings.max_width,
-			condition04 = settings.min_width==0 && settings.max_width==0;
+			var condition01 = settings.min_width!=0 && settings.max_width!=0 && $(window).innerWidth()>=settings.min_width && $(window).innerWidth()<=settings.max_width,
+				condition02 = settings.min_width!=0 && settings.max_width==0 && $(window).innerWidth()>=settings.min_width,
+				condition03 = settings.min_width==0 && settings.max_width!=0 && $(window).innerWidth()<=settings.max_width,
+				condition04 = settings.min_width==0 && settings.max_width==0;
 
-		if(condition01 || condition02 || condition03 || condition04){
-			this.each(function() {
-	    		$(this).height("")
-				if(init <= $(this).height() && settings.referer == "max" || init >= $(this).height() && settings.referer == "min")
-					init = $(this).height();
-			})
+			if(condition01 || condition02 || condition03 || condition04){
+				element.each(function() {
+		    		$(this).height("")
+					if(init <= $(this).height() && settings.referer == "max" || init >= $(this).height() && settings.referer == "min")
+						init = $(this).height();
+				})
 
-			if(settings.dimension=="outer")
-				return this.outerHeight(init);
-			else if(settings.dimension=="inner")
-				return this.innerHeight(init);
-			else
-				return this.height(init);
+				if(settings.dimension=="outer")
+					return element.outerHeight(init);
+				else if(settings.dimension=="inner")
+					return element.innerHeight(init);
+				else
+					return element.height(init);
 
-		}else{
-			return this.height("");
-		}
+			}else{
+				return element.height("");
+			}
+		}).trigger('resize');
 	}
 })(jQuery);
